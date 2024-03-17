df6ce648f594f7add18b0c92579a977d3e334e46
Christener19 <christenerleigh7070@gmail.com>
2024-03-17 13:24:57 +0000

    better user experience with add and remove for rating

--- interactive-rating-component-main/client/index.js
+++ interactive-rating-component-main/client/index.js
@@ -7,14 +7,23 @@ let number = document.querySelectorAll(".number")
 
 second.style.display = "none";
 
+
 submitBtn.addEventListener("click", function() {
     first.style.display = "none"
     second.style.display = "block"
      
 })
 
-number.forEach(function(chosen) {
-chosen.addEventListener("click", function (){
-  rating.innerHTML = chosen.innerHTML
+number.forEach(function(chosen){
+    chosen.addEventListener("click", function() {
+    number.forEach(function(clicked) {
+        if (chosen !== clicked) {
+        clicked.classList.remove("active")
+        }
+    })
+    chosen.classList.add("active")
+
+    rating.innerHTML = chosen.textContent
 })
+
 })
